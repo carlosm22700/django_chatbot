b11b45dc5b5cc0ddacd455de7b50da5a673e6d9c
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-15 23:29:31 -0700

    implements auth views

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.chatbot, name='chatbot'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout')
 ]
--- chatbot/views.py
+++ chatbot/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.contrib import auth
+from django.contrib.auth.models import User
 import openai
 import os
 
@@ -27,3 +29,44 @@ def chatbot(request):
         response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('chatbot')
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            try:
+                user = User.objects.create_user(username, email, password1)
+                user.save()
+                auth.login(request, user)
+                return redirect('chatbot')
+            except:
+                error_message = 'Error Creating Account'
+        else:
+            error_message = "Passwords Don't Match"
+            return render(request, 'register.html', {'error_message': error_message})
+    return render(request, 'register.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
